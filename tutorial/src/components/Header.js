78e47608f1fd4d93bcea11e74cba3629f391b58a
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2022-05-19 07:25:03 +0100

    committed

---
+++ tutorial/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = ()=>{
+    return(
+        <Header>
+            <h2>welcome to react course</h2>
+            <p>We will do it better.</p>
+        </Header>
+    )
+}
+export default Header
