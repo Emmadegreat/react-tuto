74c0c18e3358aee74ef2e25359eda2d44cacc13e
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2022-06-07 15:47:47 +0100

    committed

--- tutorial/src/Form.js
+++ tutorial/src/Form.js
@@ -1,7 +1,10 @@
-import React, {useState} from 'react'
-import { useForm } from 'react-hook-form'
-import 'bootstrap/dist/css/bootstrap.css'
-import * as yup from "yup"
+import 'bootstrap/dist/css/bootstrap.css';
+
+import * as yup from "yup";
+
+import React, { useState } from 'react';
+
+import { useForm } from 'react-hook-form';
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const schema = yup.object().shape({
@@ -9,7 +12,7 @@ const schema = yup.object().shape({
     email: yup.string().email().required(),
     password: yup.string().password().min(8).max(16).required(),
     confirm__password: yup.string().password().oneOf([yup.ref("password"), null]).required(),
-    number: yup.number().phone__number().min(11).max(11).required(),
+    number: yup.number().min(11).max(11).required(),
 })
 
 
@@ -23,6 +26,7 @@ const Form = () => {
 
   return (
       <div className="formApp">
+          <p>hellllllllllllllllllow</p>
         <form action="#" method="post" className="form" onSubmit={handleSubmit(submitForm)} >
               <h1>Sign Up</h1>
               <label htmlFor="fullname">Fullname</label><br />
@@ -81,82 +85,3 @@ const Form = () => {
 export default Form
 
 
-/*
-
-
-<div className="formApp">
-          <form autoComplete="off" action="#" method="post" className="form" onSubmit={handleSubmit((data)=>console.log(data))} >
-              <h1>Sign Up</h1>
-              <label htmlFor="fullname">Fullname</label><br />
-              <input
-                  type="text"
-                  {...register("fullname", {
-                      required: "fullname is required",
-                      minLength: {
-                          value: 3,
-                          message: "3 minimum characters",
-                      },
-                      maxLength: {
-                          value: 10,
-                          message: "not more than 10 characters",
-                      },
-                  })}
-                  name="full__name"
-                  id="full__name"
-                  placeholder="fullname"
-              /> <p>{ errors.fullname?.message}</p> 
-
-                <label htmlFor="email">Email</label><br />
-              <input
-                  type="email"
-                  {...register("email", {
-                      required: "email required",
-                      minLength: 50, maxLength: 20,
-                      pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                      message:"Email must be valid"
-                  })}
-                  
-                  name="email"
-                  id="email"
-                  placeholder="Email"
-              /> <p>{ errors.email?.message}</p>
-              
-              <label htmlFor="password">Password</label><br />
-              <input
-                  type="password"
-                  {...register("password", {
-                      required: "password required",
-                      maxLength: 16,
-                      minLength: 8,
-                      pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,16})$/,
-                      message:"Email must conatin atleast 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 special case character"
-                  })}
-                  name="password"
-                  id="user__password"
-                  placeholder="Password"
-              /> <p>{ errors.password?.message}</p>
-
-              <label htmlFor="confirm__password">Confirm__password</label><br />
-              <input
-                  type="password"
-                  {...register("confirm__password", {
-                      required: "password required",
-                      message:"password must match"
-                  })}
-                  name="password"
-                  id="confirm__password"
-                  placeholder="Confirm__password"
-              />
-              <p>{ errors.confirm__password?.message}</p>
-
-              <select name="" id="" {...register("gender", {required:"gender is required"})}>
-                  <option value="">Male</option>
-                  <option value="">Female</option>
-              </select>
-              <p>{ errors.gnder?.message}</p><br />
-
-                <input type="submit" className="btn btn-primary" value="sign up" />
-          </form>
-
-
-*/
--- tutorial/src/components/hooks.js
+++ tutorial/src/components/hooks.js
@@ -88,3 +88,83 @@ const Hooks = () => {
 }
 
 export default Hooks
+
+/*
+
+
+<div className="formApp">
+          <form autoComplete="off" action="#" method="post" className="form" onSubmit={handleSubmit((data)=>console.log(data))} >
+              <h1>Sign Up</h1>
+              <label htmlFor="fullname">Fullname</label><br />
+              <input
+                  type="text"
+                  {...register("fullname", {
+                      required: "fullname is required",
+                      minLength: {
+                          value: 3,
+                          message: "3 minimum characters",
+                      },
+                      maxLength: {
+                          value: 10,
+                          message: "not more than 10 characters",
+                      },
+                  })}
+                  name="full__name"
+                  id="full__name"
+                  placeholder="fullname"
+              /> <p>{ errors.fullname?.message}</p> 
+
+                <label htmlFor="email">Email</label><br />
+              <input
+                  type="email"
+                  {...register("email", {
+                      required: "email required",
+                      minLength: 50, maxLength: 20,
+                      pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                      message:"Email must be valid"
+                  })}
+                  
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+              /> <p>{ errors.email?.message}</p>
+              
+              <label htmlFor="password">Password</label><br />
+              <input
+                  type="password"
+                  {...register("password", {
+                      required: "password required",
+                      maxLength: 16,
+                      minLength: 8,
+                      pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,16})$/,
+                      message:"Email must conatin atleast 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 special case character"
+                  })}
+                  name="password"
+                  id="user__password"
+                  placeholder="Password"
+              /> <p>{ errors.password?.message}</p>
+
+              <label htmlFor="confirm__password">Confirm__password</label><br />
+              <input
+                  type="password"
+                  {...register("confirm__password", {
+                      required: "password required",
+                      message:"password must match"
+                  })}
+                  name="password"
+                  id="confirm__password"
+                  placeholder="Confirm__password"
+              />
+              <p>{ errors.confirm__password?.message}</p>
+
+              <select name="" id="" {...register("gender", {required:"gender is required"})}>
+                  <option value="">Male</option>
+                  <option value="">Female</option>
+              </select>
+              <p>{ errors.gnder?.message}</p><br />
+
+                <input type="submit" className="btn btn-primary" value="sign up" />
+          </form>
+
+
+*/
